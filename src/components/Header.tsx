101db4fc7c4f8ffa7bd7f56c6c1a937871e5b8f2
Akanni Emmanuel <akannie33@gmail.com>
2024-11-04 15:37:25 +0100

    refactor: give header a glass-morphism look

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,29 +6,33 @@ import Logo from "./Logo";
 export default function Header() {
   return (
     <>
-      <header className="px-4 lg:px-6 h-14 border-b-2 border-black dark:border-neutral-300 bg-white dark:bg-black dark:text-white flex items-center z-50 top-0 sticky">
-        <Logo />
-        <nav className="ml-auto flex gap-4 sm:gap-6">
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="/#features"
-          >
-            Features
-          </Link>
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="/#pricing"
-          >
-            Pricing
-          </Link>
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="/#testimonials"
-          >
-            Testimonials
-          </Link>
-          <ThemeSwitch />
-        </nav>
+      <header className="top-0 sticky bg-transparent gap-2 bg-opacity-30 backdrop-blur-md border-b border-white border-opacity-20 shadow-md z-50">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="h-16 flex items-center gap-2">
+            <Logo />
+            <nav className="ml-auto flex gap-4 sm:gap-6">
+              <Link
+                className="text-sm font-medium hover:underline underline-offset-4"
+                href="/#features"
+              >
+                Features
+              </Link>
+              <Link
+                className="text-sm font-medium hover:underline underline-offset-4"
+                href="/#pricing"
+              >
+                Pricing
+              </Link>
+              <Link
+                className="text-sm font-medium hover:underline underline-offset-4"
+                href="/#testimonials"
+              >
+                Testimonials
+              </Link>
+              <ThemeSwitch />
+            </nav>
+          </div>
+        </div>
       </header>
     </>
   );
--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -12,10 +12,14 @@ const orgs = [
 export default function DashboardHeader() {
   return (
     <>
-      <header className="px-4 lg:px-6 h-14 flex items-center border-b gap-2 top-0 z-10 bg-primary-foreground sticky">
-        <Logo />
-        <DashboardNav />
-        <ThemeSwitch />
+      <header className="top-0 sticky bg-transparent gap-2 bg-opacity-30 backdrop-blur-md border-b border-white border-opacity-20 shadow-md z-50">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center h-16 gap-2">
+            <Logo />
+            <DashboardNav />
+            <ThemeSwitch />
+          </div>
+        </div>
       </header>
     </>
   );
